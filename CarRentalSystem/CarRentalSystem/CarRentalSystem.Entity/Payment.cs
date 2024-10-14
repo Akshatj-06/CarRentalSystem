aaac1cdea90fb5f2213f034f4e7c0c917150e63c
Akshatj-06 <akshatjain902@gmail.com>
2024-10-14 11:23:42 +0530

    Add files via upload

---
+++ CarRentalSystem/CarRentalSystem/CarRentalSystem.Entity/Payment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalSystem.Entity
+{
+    public class Payment
+    {
+        public int paymentID { get; set; }
+        public int leaseID { get; set; }
+        public DateTime paymentDate { get; set; }
+        public int amount{ get; set; }
+    }
+}
